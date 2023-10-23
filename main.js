d32db85ecaa76f7de6ec585f84bc289077b40236
David Contreras <dvd.cnt@gmail.com>
2023-10-23 17:09:12 +0000

    Included alpine logo and data

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import './style.css';
-import javascriptLogo from './javascript.svg';
+import alpineLogo from './alpine_long.svg';
 import viteLogo from '/vite.svg';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.data('app', () => ({
   viteLogo,
-  javascriptLogo,
+  alpineLogo,
   count: 0,
 })
 )
